861ae5abe189c2ada76d8a4e924d16a65ad6d1c6
Nicolas Anderson <nicolas-31@outlook.com>
2024-07-28 12:24:23 +0200

    updated lab 2 and 3 to .net 8 and latest nuget packages

--- Labfiles/05-custom-entity-recognition/C-Sharp/custom-entities/obj/Debug/net7.0/custom-entities.AssemblyInfo.cs
+++ Labfiles/05-custom-entity-recognition/C-Sharp/custom-entities/obj/Debug/net7.0/custom-entities.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("custom-entities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f746c47b9b13faef7225a38a31278cf8cf66bdf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf60b8906530d09ec5bb85442390b702105b5d1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("custom-entities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("custom-entities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
