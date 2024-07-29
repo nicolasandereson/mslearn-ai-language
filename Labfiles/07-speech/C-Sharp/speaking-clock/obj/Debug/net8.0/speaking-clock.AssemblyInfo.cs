aa6e64af1261ff36820ea5a0bd1bdbeb8f5d8c78
Nicolas Anderson <nicolas-31@outlook.com>
2024-07-29 22:06:47 +0200

    have fixed lab5

--- Labfiles/07-speech/C-Sharp/speaking-clock/obj/Debug/net8.0/speaking-clock.AssemblyInfo.cs
+++ Labfiles/07-speech/C-Sharp/speaking-clock/obj/Debug/net8.0/speaking-clock.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("speaking-clock")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23c48c61f9d67b01b5ff5a5ebbd07070aee3dfbd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74986de78ca070e373da4f20b0f04a0250c4b936")]
 [assembly: System.Reflection.AssemblyProductAttribute("speaking-clock")]
 [assembly: System.Reflection.AssemblyTitleAttribute("speaking-clock")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
